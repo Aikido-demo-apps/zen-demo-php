04652b3b017529f45afa5fa2cf6a26e6209be256
Samuel <samuel@sava.be>
2025-04-07 16:25:01 +0200

    fixed middleware order

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $kernel = $this->app->make(Kernel::class);
-        $kernel->pushMiddleware(AikidoMiddleware::class);
         $kernel->pushMiddleware(UserMiddleware::class);
+        $kernel->pushMiddleware(AikidoMiddleware::class);
     }
 }
