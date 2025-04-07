6c68156faa2b62ab5ffadabfd6b738f515088a43
Samuel <samuel@sava.be>
2025-04-07 13:29:09 +0200

    fix couple of issues

--- .fly/scripts/aikido.sh
+++ .fly/scripts/aikido.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd /tmp
+cd /tmp || exit
 
 curl -L -O https://github.com/AikidoSec/firewall-php/releases/download/v1.0.117/aikido-php-firewall.x86_64.deb
 dpkg -i -E ./aikido-php-firewall.x86_64.deb
