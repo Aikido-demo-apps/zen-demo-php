12eb6f59bb418eae93ddafd267b163eb66ae9bce
Marian Popovici <marain@aikido.dev>
2025-07-30 16:14:09 +0300

    sync

--- .fly/scripts/aikido.sh
+++ .fly/scripts/aikido.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd /tmp || exit
-curl -L -O "https://github.com/AikidoSec/firewall-php/releases/download/v${AIKIDO_VERSION}/aikido-php-firewall.$(uname -i).deb"
+
+curl -L -O "https://github.com/AikidoSec/firewall-php/releases/download/v1.0.126/aikido-php-firewall.$(uname -i).deb"
 dpkg -i -E "./aikido-php-firewall.$(uname -i).deb"
