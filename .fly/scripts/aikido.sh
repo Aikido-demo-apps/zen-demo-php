24409f4583024981c0b672207b5afbce52433ee5
Tudor TIMCU <tudor@aikido.dev>
2025-08-02 20:44:11 +0300

    1.1.0

--- .fly/scripts/aikido.sh
+++ .fly/scripts/aikido.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /tmp || exit
 
-curl -L -O "https://github.com/AikidoSec/firewall-php/releases/download/v1.0.126/aikido-php-firewall.$(uname -i).deb"
+curl -L -O "https://github.com/AikidoSec/firewall-php/releases/download/v1.1.0/aikido-php-firewall.$(uname -i).deb"
 dpkg -i -E "./aikido-php-firewall.$(uname -i).deb"
